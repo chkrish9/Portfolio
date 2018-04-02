1d813cce555df2138c355998adf1c97db934496f
Murali <murali.24.4.91@gmail.com>
2018-04-02 22:54:41 +0530

    modification in contact page

--- js/script.js
+++ js/script.js
@@ -41,20 +41,24 @@
       $("#error-block").show();
       return false;
     } else {
-      $.ajax({
-        method: 'POST',
-        url: '//formspree.io/murali.ofc@outlook.com',
-        data: $('#contact-form').serialize(),
-        datatype: 'json',
-        success:function(data) {
-          console.log(data); 
-          $("#captcha").html("");
-          $("#captcha").html(data);
-          $("#passthrough").submit();
-          $("#captcha").html("");
-        }   
-      });
       e.preventDefault();
+    $.ajax({
+      url: '//formspree.io/murali.ofc@outlook.com',
+      method: 'POST',
+      data: $(this).serialize(),
+      dataType: 'json',
+      beforeSend: function() {
+        $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+      },
+      success: function(data) {
+        $contactForm.find('.alert--loading').hide();
+        $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+      },
+      error: function(err) {
+        $contactForm.find('.alert--loading').hide();
+        $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+      }
+    });
       $(this).get(0).reset();
       $("#error-block").hide();
     }
